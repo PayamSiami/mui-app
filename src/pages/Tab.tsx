0dc3715bf67519e5ee286453050d19bd9035a75e
payam <ps@qaap.io>
2023-02-28 18:48:12 +0330

    add Tab

---
+++ src/pages/Tab.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import VerticalTab from "../components/tab/VerticalTab";
+import { Box, Typography } from "@mui/material";
+import ImageAvatar from "../components/avatar/Avatar";
+
+export default function Tab() {
+  return (
+    <>
+      <Box sx={{ display: "flex", alignItems: "center" }}>
+        <Box sx={{ p: 3 }}>
+          <ImageAvatar sx={{ width: 62, height: 62 }} />
+        </Box>
+        <Box sx={{ display: "flow-root" }}>
+          <Typography variant="h5" gutterBottom>
+            سعید صفایی
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            بدون اشتراک
+          </Typography>
+        </Box>
+      </Box>
+      <Box sx={{ flex: 1, width: "100%" }}>
+        <VerticalTab />
+      </Box>
+    </>
+  );
+}
