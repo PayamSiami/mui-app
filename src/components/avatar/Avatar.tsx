ee10649a580b90ad44430ad20b049cea6ea9d450
payam <ps@qaap.io>
2023-02-28 18:44:43 +0330

    add dayjs and @mui/x-date-pickers package

--- src/components/avatar/Avatar.tsx
+++ src/components/avatar/Avatar.tsx
@@ -1,15 +1,17 @@
-import * as React from "react";
 import Avatar from "@mui/material/Avatar";
 import Stack from "@mui/material/Stack";
+import { SxProps } from "@mui/material";
 
-export default function ImageAvatar() {
+type Props = {
+  sx?: SxProps<any> | undefined;
+};
+
+export default function ImageAvatar(props: Props) {
+  const { sx, ...other } = props;
+
   return (
     <Stack direction="row" spacing={2}>
-      <Avatar
-        alt="Remy Sharp"
-        src="/images/1.jpg"
-        sx={{ width: 56, height: 56 }}
-      />
+      <Avatar sx={{ ...sx }} alt="Remy Sharp" src="/images/1.jpg" {...other} />
     </Stack>
   );
 }
