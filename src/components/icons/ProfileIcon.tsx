8c785bbe502ed52dc1715430a80b7465130455e0
payam <ps@qaap.io>
2023-02-28 12:46:31 +0330

    add components

---
+++ src/components/icons/ProfileIcon.tsx
@@ -0,0 +1,24 @@
+import { SvgIcon } from "@mui/material";
+import React from "react";
+
+function ProfileIcon() {
+  return (
+    <SvgIcon
+      width="16"
+      height="9"
+      viewBox="0 0 16 9"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M14.74 5.38006C12.96 7.01006 10.6 8.00006 8.00001 8.00006C5.40001 8.00006 3.04001 7.01006 1.26001 5.38006C1.36001 4.44006 1.96001 3.52006 3.03001 2.80006C5.77001 0.980059 10.25 0.980059 12.97 2.80006C14.04 3.52006 14.64 4.44006 14.74 5.38006Z"
+        stroke="#292D32"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </SvgIcon>
+  );
+}
+
+export default ProfileIcon;
