a4f17bc3079813c6103d3eb3cfb859ad3df4b610
payam <ps@qaap.io>
2023-02-28 14:57:58 +0330

    edit theme

--- src/theme.ts
+++ src/theme.ts
@@ -1,20 +1,28 @@
-import { red } from '@mui/material/colors';
-import { createTheme } from '@mui/material/styles';
+import { red } from "@mui/material/colors";
+import { createTheme } from "@mui/material/styles";
 
 // Create a theme instance.
 const theme = createTheme({
   palette: {
     primary: {
-      main: '#556cd6',
+      main: "#556cd6",
+      contrastText: "#fff",
+      light: "#B0BF24",
+      dark: "#3F4254",
     },
     secondary: {
-      main: '#19857b',
+      main: "#19857b",
+      contrastText: "#fff",
+      light: "#19857b",
+      dark: "#00695c",
     },
     error: {
       main: red.A400,
+      contrastText: "#fff",
+      light: red.A400,
+      dark: red.A400,
     },
   },
 });
 
 export default theme;
-
