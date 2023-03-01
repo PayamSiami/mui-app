9247b391b63140a52345dd5a40998473ddaff01f
payam <ps@qaap.io>
2023-03-02 00:05:38 +0330

    add index

---
+++ src/components/index.ts
@@ -0,0 +1,9 @@
+export { default as Autocomplete } from "./Autocomplete/Autocomplete";
+export { default as Avatar } from "./Avatar/Avatar";
+export { default as Button } from "./Button/Button";
+export { default as IconButton } from "./Button/IconButton";
+export { default as Checkbox } from "./Checkbox/Checkbox";
+export { default as DatePicker } from "./DatePicker/DatePicker";
+export { default as Input } from "./Input/Input";
+export { default as MobileTab } from "./Tab/MobileTab";
+export { default as VerticalTab } from "./Tab/VerticalTab";
---
+++ src/pages/index.ts
@@ -0,0 +1,3 @@
+export { default as Dashboard } from './Dashboard';
+export { default as Tab } from './Tab';
+export { default as Profile } from './Profile';
