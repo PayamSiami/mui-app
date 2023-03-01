d8b59046e9ec158c1d5e7f2e28180174f317ef95
payam <ps@qaap.io>
2023-03-02 00:06:53 +0330

    edit Profile

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,46 +1,73 @@
-import ImageAvatar from "../components/avatar/Avatar";
-import { Box, Grid } from "@mui/material";
-import DatePicker from "../components/datePicker/DatePicker";
-import Input from "../components/input/Input";
-import Autocomplete from "../components/Autocomplete/Autocomplete";
-import Checkboxes from "../components/Checkbox/Checkbox";
-import Button from "../components/Button/Button";
+import { Box, Grid, Typography, useMediaQuery } from "@mui/material";
+
+import {
+  Autocomplete,
+  Avatar,
+  Button,
+  Checkbox,
+  DatePicker,
+  IconButton,
+  Input,
+} from "../components";
 
 export default function Profile() {
+  const isSmallScreen = useMediaQuery((theme: any) =>
+    theme.breakpoints.down("sm")
+  );
+
   return (
     <Grid container spacing={3}>
-      <Grid item xs={6}>
-        <Box>
-          <Box padding={2}>
-            <ImageAvatar sx={{ width: 104, height: 104 }} />
+      <Grid item xs={isSmallScreen ? 12 : 6}>
+        <Box
+          sx={{
+            position: isSmallScreen ? "relative" : "absolute",
+            top: isSmallScreen ? 0 : 66,
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <img
+            alt={"emoji"}
+            width={24}
+            height={24}
+            loading="lazy"
+            src={"/images/emoji.png"}
+          />
+          <Typography variant="subtitle1" gutterBottom mr={1}>
+            ویرایش پروفایل
+          </Typography>
+        </Box>
+        <Box padding={2} display={"flex"} justifyContent={"center"}>
+          <Avatar sx={{ width: 104, height: 104 }} />
+          <Box position={"absolute"} mt={8} ml={8}>
+            <IconButton />
           </Box>
-          <Box mb={1}>
-            <Input placeholder="نام خود را وارد کنید."/>
+        </Box>
+        <Box mb={1}>
+          <Input placeholder="نام خود را وارد کنید." />
+        </Box>
+        <Box mb={1}>
+          <DatePicker />
+        </Box>
+        <Box mb={1}>
+          <Autocomplete />
+        </Box>
+        <Grid item xs={isSmallScreen ? 8 : 6}>
+          <Box display={"flex"} justifyContent={"space-between"}>
+            <Checkbox label="مجرد" />
+            <Checkbox label="متاهل" />
           </Box>
-          <Box mb={1}>
-            <DatePicker />
+        </Grid>
+        <Grid item xs={isSmallScreen ? 8 : 6}>
+          <Box display={"flex"} justifyContent={"space-between"}>
+            <Checkbox label="خانه دار" />
+            <Checkbox label="شاغل" />
           </Box>
-          <Box mb={1}>
-            <Autocomplete />
-          </Box>
-          <Grid item xs={6}>
-            <Box display={"flex"} justifyContent={"space-between"}>
-              <Checkboxes label="مجرد" />
-              <Checkboxes label="متاهل" />
-            </Box>
-          </Grid>
-          <Grid item xs={6}>
-            <Box display={"flex"} justifyContent={"space-between"}>
-              <Checkboxes label="خانه دار" />
-              <Checkboxes label="شاغل" />
-            </Box>
-          </Grid>
-          <Grid item xs={6}>
-            <Box display={"flex"} justifyContent={"space-between"}>
-              <Button title="ذخیره اطلاعات" variant="contained" />
-              <Button title="انصراف" variant="outlined" />
-            </Box>
-          </Grid>
+        </Grid>
+        <Box display={"flex"} justifyContent={"space-between"}>
+          <Button title="ذخیره اطلاعات" variant="contained" />
+          <Button title="انصراف" variant="outlined" />
         </Box>
       </Grid>
     </Grid>
